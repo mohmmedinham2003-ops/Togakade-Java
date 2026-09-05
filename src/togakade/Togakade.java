820e4dd95492c215324b5e9ef9977b797ee698ab
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-09-05 15:07:02 +0530

    database connection commit

--- src/togakade/Togakade.java
+++ src/togakade/Togakade.java
@@ -4,6 +4,12 @@
  */
 package togakade;
 
+import com.sun.jdi.connect.spi.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Inham
@@ -14,7 +20,13 @@ public class Togakade {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hi");
-     }
-    
+        try {
+            java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/companydb", "root","123456789");
+            System.out.println(connection);
+        } catch (SQLException ex) {
+            Logger.getLogger(Togakade.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex);
+        }
+    }
+
 }
