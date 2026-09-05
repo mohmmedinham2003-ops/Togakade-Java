81422abf7a259b869c4eb248381ddab41f09c029
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-09-05 16:18:08 +0530

    Done database first step

---
+++ src/model/Employee.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Inham
+ */
+public class Employee {
+    private integer emp_id;
+    private String emp_name;
+    private String designation;
+    private double salary;
+    private integer dept_id;
+    private integer manager_id;
+
+    
+     
+}
