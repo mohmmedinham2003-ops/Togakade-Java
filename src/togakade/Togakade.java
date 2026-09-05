415a43bfb21a5964cde45c1582c348e3ac2a9681
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-09-05 15:54:18 +0530

    Done

--- src/togakade/Togakade.java
+++ src/togakade/Togakade.java
@@ -6,7 +6,9 @@ package togakade;
 
 import com.sun.jdi.connect.spi.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,6 +26,20 @@ public class Togakade {
             java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/companydb", "root","123456789");
             System.out.println(connection);
             String sql = "SELECT * FROM employee";
+            Statement stm = connection.createStatement();
+            ResultSet resultset = stm.executeQuery(sql);
+            System.out.println(resultset);
+            boolean next = resultset.next();
+            while(resultset.next()){
+            String name = resultset.getString(2);
+            String dec = resultset.getString(3);
+            double salary = resultset.getDouble(4);
+            System.out.println(name+":"+dec+"| Salary :"+salary);
+            }
+            
+//            while(resultset.next()){
+//            
+//            }
         } catch (SQLException ex) {
             Logger.getLogger(Togakade.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println(ex);
